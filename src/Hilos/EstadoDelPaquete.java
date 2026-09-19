d8fda7c2c63ba56f6d6add6b2d3c0f8cae15c39d
Ian Suazo <ianantoniosuazo@gmail.com>
2026-09-19 08:27:06 -0600

    Se crea la clase de EstadoPaquete como enum para dar las diferentes clasificaciones que tendra los paquetes y sus envios

---
+++ src/Hilos/EstadoDelPaquete.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hilos;
+
+/**
+ *
+ * @author Ian Suazo Palao
+ */
+public enum EstadoDelPaquete {
+    RECIBIDO,ALMACENADO,CLASIFICANDO,CLASIFICADO,EMPAQUETANDO,EMPAQUETADO,EN_EXPEDICION,EN_REPARTO,ENTREGADO,NUEVO_INTENTO,DEVUELTO
+}
